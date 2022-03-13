b58eda69ecb90007956dc7559b68b948962ee015
CamiloCJ09 <camilocampazj@gmail.com>
2022-03-12 20:33:28 -0500

    merge

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,4 +1,13 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Controller {
+    ArrayList<Vertice<String>> states;
+    ArrayList<Edge<Vertice<String>>> transitions;
+
+    public Controller(){
+        states = new ArrayList<>();
+        transitions = new ArrayList<>();
+    }
 }
